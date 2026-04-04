510bdf7d57ba20ceca27f60d34a9070fc81cdada
Lars Risdal <larsrisdal@gmail.com>
2026-04-04 15:15:32 +0200

    restore window-state for settings

--- src/claudio-desktop/src/lib.rs
+++ src/claudio-desktop/src/lib.rs
@@ -37,11 +37,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(
-            tauri_plugin_window_state::Builder::default()
-                .with_denylist(&["settings"])
-                .build(),
-        )
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             commands::games::cancel_install,
             commands::games::get_installed_game,
